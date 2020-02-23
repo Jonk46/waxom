98fa6789b10f1f466c16f20dac4c923aad3dd7ad
Alex <jonk6233@gmail.com>
2020-02-23 12:17:23 +0200

    isotop js

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var swiper = new Swiper('.swiper-container', {
+	cssMode: true,
+	autoplay: {
+    delay: 3000,
+  },
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	mousewheel: true,
+	keyboard: true,
+});
+
+
+/////////isotop
+
+var $grid = $('.grid').isotope({
+	itemSelector: '.grid-item',
+	stagger: 30
+});
+
+$('.filter-button-group').on( 'click', '.button', function() {
+	var filterValue = $(this).attr('data-filter');
+	$grid.isotope({ filter: filterValue });
+});
+
+////// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+	
+ var $buttonGroup = $( buttonGroup );
+ $buttonGroup.on( 'click', 'button', function() {
+	 $buttonGroup.find('.is-checked').removeClass('is-checked');
+	 $( this ).addClass('is-checked');
+ });
+});
